08b9e01f5d8e38a78ba517af80d864f941bec9a5
Dadui93 <sunhx1993@gmail.com>
2016-04-05 08:13:19 -0400

    <haoxiangsun>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
   public static void main(String[] args) {
 
-   port(Integer.valueOf(System.getenv("PORT")));
+   //port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 	 get("/hello", (req, res) -> "Hello World");
     //Object r = new JavaGettingStarted();
--- src/main/java/routes/RestfulRoutes.java
+++ src/main/java/routes/RestfulRoutes.java
@@ -11,8 +11,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-
+import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -50,34 +52,54 @@ public class RestfulRoutes {
                 // message
                 attributes.put("message", "Hello World!!!!!");
                 // lst
-        		List list=new ArrayList();
-        		int k = 3;
-        		for (int i=0; i<k; i++) {
-        			list.add("please give me some advices!!!!");
-        		}
-        		attributes.put("lst", list);
-        		
-        		attributes.put("Me", "I am Haoxiang Sun");
-        		
-        		
-        		
-        		
+                List list=new ArrayList();
+                int k = 3;
+                for (int i=0; i<k; i++) {
+                    list.add("please give me some advices!!!!");
+                }
+                attributes.put("lst", list);
+                
+                attributes.put("Me", "I am Haoxiang Sun");
+                
+                
+                
+                
                 return new ModelAndView(attributes, "home.ftl");
             }, new FreeMarkerEngine());
             
             
             get("/api/my_info", (req, res) -> {
+                List<Object> data2 =new ArrayList<>();
+                Connection connection=null;
+                try{
+                connection = DatabaseUrl.extract().getConnection();
+                Statement stmt = connection.createStatement();
+                ResultSet rs = stmt.executeQuery("");
+                
+                while(rs.next()){
                 Map<String, Object> data = new HashMap<>();
-                data.put("Name", "Haoxiang Sun");
-                data.put("Phone", "412-652-7800");
-                data.put("Email", "has129@pitt.edu");
-                return data;
+                data.put("Name", rs.getString("NAME"));
+                data.put("Phone", rs.getString("PHONE"));
+                data.put("Email", rs.getString("EMAIL"));
+                data2.add(data);
+                }
+            }catch (Exception e){
+                data2.add("error"+e);
+            }finally{
+                if(connection !=null)
+                    try{
+                        connection.close();
+                    }catch(SQLException e){
+                        
+                    }
+            }
+                return data2;
             }, gson::toJson);
             
             
             post("/api/add_music_info", (req, res) -> {
-            	String info = req.queryParams("info");
-            	
+                String info = req.queryParams("info");
+                
                 Map<String, Object> data = new HashMap<>();
                 data.put("info", info);
                 data.put("status", "OK");
@@ -99,32 +121,32 @@ public class RestfulRoutes {
     }
                 
     public String readXML(String file) throws IOException {
-    	BufferedReader br = null;
-		br = new BufferedReader(new FileReader(file));
-	    StringBuilder sb = new StringBuilder();
-	    String line = br.readLine();
+        BufferedReader br = null;
+        br = new BufferedReader(new FileReader(file));
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
 
-	    while (line != null) {
-	        sb.append(line);
-	        sb.append(System.lineSeparator());
-	        line = br.readLine();
-	    }
-	    String everything = sb.toString();
-	    br.close();
-	    return everything;
+        while (line != null) {
+            sb.append(line);
+            sb.append(System.lineSeparator());
+            line = br.readLine();
+        }
+        String everything = sb.toString();
+        br.close();
+        return everything;
 
     }
     
 }
 
 class MyInfo {
-	public String Name;
-	public String Phone;
-	public String Email;
-	
-	public MyInfo(String a, String p, String e) {
-		Name = a;
-		Phone = p;
-		Email = e;
-	}
+    public String Name;
+    public String Phone;
+    public String Email;
+    
+    public MyInfo(String a, String p, String e) {
+        Name = a;
+        Phone = p;
+        Email = e;
+    }
 }
